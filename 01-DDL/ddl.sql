2c2a8e3665877d0f41168b851b6142c5e3c33a6d
itsnotm3h L <thewhit3pages@gmail.com>
2024-10-12 08:18:47 +0000

    lesson-1

---
+++ 01-DDL/ddl.sql
@@ -0,0 +1,97 @@
+-- you can have more than 1 database work at the same time --
+-- See all the database on your server --
+show databases;
+
+-- create a new database --
+-- name after the domain --
+create database swimming_coach;
+
+-- possible for a database that your cant see the interface -- 
+
+-- Change active database -- 
+-- use <nameOfDatabase> --
+use swimming_coach;
+
+-- We are learning MYSQL (SME and Startup) --
+-- they are other SQL providers, Oracle (Oldest, use by big enterprises), postGres(Json), microsoft and mariaDB (From techies that left Oracle) --
+-- if an item is open source, you wont be able to close it up --
+-- MariaDB = MYSQL  --
+
+
+-- Create new table --
+-- Named `parents` with some columns 
+-- snytax: create table <table_name> (
+-- <col name> <col data type> <options>,
+--) engine = innodb;
+-- the engine is the enable forigen keys.
+create table parents(
+    parent_id int unsigned auto_increment primary key,
+    first_name varchar(200) not null,
+    last_name varchar(200) default ''
+) engine = innodb;
+
+-- show the table -- 
+show tables;
+
+-- 2 way to create tables with foriegn key. --
+-- option 1. create table then add in foriegn key. --
+-- option 2. create table and foriegn key at the same time. ---
+-- MYSQL command is not case sensitive --
+
+-- option 1 --
+create table student (
+    student_id mediumint unsigned auto_increment primary key,
+    name varchar(200) not null,
+    date_of_birth datetime not null
+) engine = innodb;
+
+-- ALTER TABLE --
+-- to alter an exisiting table, change coliumn, add a new colum or delete the col -- 
+-- ** if you are going to add a foriegn key to another table. the datatype must match -- 
+
+-- To create a column.
+-- MUST MAKE SURE THAT THE FOREIGN KEY MUST BE THE SAME AS WHAT YOU WANT TO ADD -- 
+alter table student add column parent_id int unsigned;
+
+-- To add a foriegn key, definie parent id in student table as foriegn key --
+-- fk_students_parents (this is defined by us, is the name of the foreign key. --
+-- Set the foreign key name, refer it to the parent id that is found in the parents table called parents_id --
+-- it will show that its a foreign key as MUL.
+ALTER TABLE student ADD CONSTRAINT fk_students_parents FOREIGN KEY (parent_id) REFERENCES parents(parent_id);
+
+
+-- ALTER TABLE: Modify an existing column --
+-- make the parent_id not null --
+-- must provide new column defintiion, myst provide the datatype.
+ALTER TABLE student MODIFY COLUMN parent_id INT UNSIGNED NOT NULL;
+
+
+-- INSERT: Add new data --
+-- Syntax: INSERT INTO <table name> (<col1>,<col2>) VALUES (<col1>,<col2>)
+-- IT is by the order of what you have define in ();
+INSERT INTO parents (first_name,last_name) VALUES ("John","Wick");
+
+-- See all rows from table --
+-- Syntax: SELECT <*, means all> FROM <table name>;
+SELECT * FROM parents;
+
+
+-- what happens if we did not enter full data to insert?
+-- if there is a value that is not null and you did not include in the insert,it will not work. 
+-- It will not work when your parent_id does not exist. 
+-- will see an error: Cannot add or update a child row.
+INSERT INTO student (name, date_of_birth, parent_id) VALUES ("John Wick","1982-06-13",1);
+
+
+--DELETE: remove a row--
+-- the following will not work:-- 
+-- it will nnot work cause there is a table that is refering to it. 
+-- MUST HAVE A WHERE!!!!!!!! if not it will delete all the database, with no backup. 
+DELETE FROM parents WHERE parent_id = 1;
+
+-- DROP: delete a table -- 
+DROP TABLE parents;
+
+
+-- use DESCRIBE to show fields and definition of a table--
+DESCRIBE student;
